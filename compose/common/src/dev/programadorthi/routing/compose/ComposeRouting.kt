eb41452c9d4177f8df8776252d3e61393d9f92af
Thiago Santos <programadorthi@gmail.com>
2023-12-22 11:29:21 -0300

    feat: compose routing with auto disposable support

--- compose/common/src/dev/programadorthi/routing/compose/ComposeRouting.kt
+++ compose/common/src/dev/programadorthi/routing/compose/ComposeRouting.kt
@@ -1,22 +1,69 @@
 package dev.programadorthi.routing.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
+import dev.programadorthi.routing.core.Route
 import dev.programadorthi.routing.core.Routing
 import dev.programadorthi.routing.core.application
+import dev.programadorthi.routing.core.routing
+import io.ktor.util.logging.KtorSimpleLogger
+import io.ktor.util.logging.Logger
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+public val LocalRouting: ProvidableCompositionLocal<Routing?> = staticCompositionLocalOf { null }
 
 @Composable
 public fun Routing(
     routing: Routing,
     initial: Content,
 ) {
-    val composable by remember(routing) {
-        mutableStateOf(initial).also {
-            routing.application.contentState = it
+    CompositionLocalProvider(LocalRouting provides routing) {
+        val composable by remember(routing) {
+            mutableStateOf(initial).also {
+                routing.application.contentState = it
+            }
+        }
+
+        composable()
+    }
+}
+
+@Composable
+public fun Routing(
+    rootPath: String = "/",
+    parent: Routing? = null,
+    coroutineContext: CoroutineContext = EmptyCoroutineContext,
+    log: Logger = KtorSimpleLogger("kotlin-routing"),
+    developmentMode: Boolean = false,
+    configuration: Route.() -> Unit,
+    initial: Content,
+) {
+    val routing = remember {
+        routing(
+            rootPath = rootPath,
+            parent = parent,
+            parentCoroutineContext = coroutineContext,
+            log = log,
+            developmentMode = developmentMode,
+            configuration = configuration,
+        )
+    }
+
+    DisposableEffect(routing) {
+        onDispose {
+            routing.dispose()
         }
     }
 
-    composable()
+    Routing(
+        routing = routing,
+        initial = initial,
+    )
 }
