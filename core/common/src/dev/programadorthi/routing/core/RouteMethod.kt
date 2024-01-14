dc83494e06736c47f3c38d96aa927492e43988fa
Thiago Santos <programadorthi@gmail.com>
2024-01-14 17:57:43 -0300

    chore: removed pop route method

--- core/common/src/dev/programadorthi/routing/core/RouteMethod.kt
+++ core/common/src/dev/programadorthi/routing/core/RouteMethod.kt
@@ -5,14 +5,12 @@ public interface RouteMethod {
 
     public companion object {
         public val Empty: RouteMethod = RouteMethod("EMPTY")
-        public val Pop: RouteMethod = RouteMethod("POP")
         public val Push: RouteMethod = RouteMethod("PUSH")
         public val Replace: RouteMethod = RouteMethod("REPLACE")
         public val ReplaceAll: RouteMethod = RouteMethod("REPLACE_ALL")
 
         public fun parse(method: String): RouteMethod {
             return when (method) {
-                Pop.value -> Pop
                 Push.value -> Push
                 Replace.value -> Replace
                 ReplaceAll.value -> ReplaceAll
